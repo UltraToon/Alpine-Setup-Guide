9299bcc7fad6b2a30a675640a7e7ff97c69dd11c
Ivan <92064752+UltraToon@users.noreply.github.com>
2024-12-28 22:03:34 -0800

    Add files via upload

--- btrfs_alpine.sh
+++ btrfs_alpine.sh
@@ -2,7 +2,6 @@
 set -e
 MOUNTPOINT="/mnt"
 BTRFS_OPTS="defaults,ssd,noatime,space_cache=v2"
-BOOTLOADER=grub
 #compress=zstd also need to install zstd pkg
 echo ">>> Setting up..."
 apk add -q parted btrfs-progs dosfstools
@@ -19,49 +18,47 @@ read -r DISK
 # Partitioning
 parted --script -a optimal "$DISK" \
     mklabel gpt \
-    mkpart primary fat32 0% 200M \
+    mkpart primary fat32 0% 512M \
     name 1 esp \
     set 1 esp on \
-    mkpart primary btrfs 200M 100% \
+    mkpart primary btrfs 512M 100% \
     name 2 root
 
-partprobe $DISK
+partprobe "$DISK"
 
 # Partition Variable Setup
 NVME_SUFFIX=$(case "$DISK" in /dev/nvme*) echo p ;; esac)
 ESP_PAR="${DISK}${NVME_SUFFIX}1"
 BTRFS_PAR="${DISK}${NVME_SUFFIX}2"
 
-# Filesystem Format
-mkfs.vfat -F 32 "$ESP_PAR"
+
+# Formatting
+mkfs.fat -F 32 "$ESP_PAR"
 mkfs.btrfs -f -q "$BTRFS_PAR"
 
-# EFI SECTION
-mkdir -p /mnt/boot
-mount -t vfat "$ESP_PAR" /mnt/boot
-
-# BTRFS SECTION
-echo ">>> Mounting $BTRFS_PAR to $MOUNTPOINT for subvolume creation"
+# Subvolumes and Mounting
+echo ">>> Creating snapper-style subvolume layouts"
 mount -t btrfs "$BTRFS_PAR" "$MOUNTPOINT"
 SUBVOLUMES="@ @home @var_log @snapshots"
 for subvol in $SUBVOLUMES; do
     btrfs subvolume create "$MOUNTPOINT/$subvol"
 done
+btrfs subvolume set-default /mnt/@
 umount "$MOUNTPOINT"
-
 echo ">>> Unmounted $MOUNTPOINT and preparing subvolume mounts"
 mount -o subvol=@,$BTRFS_OPTS "$BTRFS_PAR" "$MOUNTPOINT"
-mkdir -p "$MOUNTPOINT/home" "$MOUNTPOINT/var/log" "$MOUNTPOINT/.snapshots"
-mount -o subvol=@home,$BTRFS_OPTS "$BTRFS_PAR" "$MOUNTPOINT/home"
-mount -o subvol=@var_log,$BTRFS_OPTS "$BTRFS_PAR" "$MOUNTPOINT/var/log"
-mount -o subvol=@snapshots,$BTRFS_OPTS "$BTRFS_PAR" "$MOUNTPOINT/.snapshots"
+mkdir -p "$MOUNTPOINT/home" "$MOUNTPOINT/var/log" "$MOUNTPOINT/.snapshots" "$MOUNTPOINT/boot"
+mount "$BTRFS_PAR" -o subvol=@home,$BTRFS_OPTS  "$MOUNTPOINT/home"
+mount "$BTRFS_PAR" -o subvol=@var_log,$BTRFS_OPTS "$MOUNTPOINT/var/log"
+mount "$BTRFS_PAR" -o subvol=@snapshots,$BTRFS_OPTS  "$MOUNTPOINT/.snapshots"
+mount "$ESP_PAR" -t vfat  /mnt/boot
 
-ROOTFS=btrfs BOOTFS=vfat BOOTLOADER=grub DISKLABEL=gpt DISKDEV=$DISK && setup-disk -s 0 -m sys /mnt
+#ROOTFS=btrfs BOOTFS=vfat BOOTLOADER=grub DISKLABEL=gpt DISKDEV=$DISK && #setup-disk -s 0 -m sys /mnt
 
-setup-disk -m sys /mnt
+BOOTLOADER=none setup-disk -k edge -m sys /mnt
 
 clear
-echo "${GREEN}>>> Script completed, please reboot. <<<${RESET}"
+echo "${GREEN}>>> Script completed, please reboot.${RESET}"
 
 ## setup fstab
 ## add chroot steps for zram, microcode, and basics
